588d9b4f27df1fcc964f659af17d5dae27428506
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-31 11:39:39 -0700

    Add rvm: to rvm prompt

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,7 +1,7 @@
 # get the name of the branch we are on
 function rvm_prompt_info() {
   ruby_version=$(~/.rvm/bin/rvm-prompt g s 2> /dev/null) || return
-  echo "$ruby_version"
+  echo "rvm:$ruby_version"
 }
 
 
